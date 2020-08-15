2a7c92603144fa206fdfc87b998fedaba8ce0256
bearbb <nguyen.b2uty@gmail.com>
2020-08-15 12:02:39 +0700

    1st commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Header } from "./components/layout/Header";
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import App from "./App";
+render(<App></App>, document.getElementById("root"));
