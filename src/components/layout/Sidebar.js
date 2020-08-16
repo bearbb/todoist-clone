30aede7c4d81dfe3f4e708ab0a5f4eb36f2c18f7
bearbb <nguyen.b2uty@gmail.com>
2020-08-16 10:44:34 +0700

    add some stuff

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,5 +1,33 @@
 import React from "react";
-
+import {
+  FaChevronDown,
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendar,
+} from "react-icons/fa";
 export const Sidebar = () => {
-  return <h1>This is side bar</h1>;
+  return (
+    <div className="sidebar" data-testid="sidebar">
+      <div className="sidebar__generic">
+        <li>
+          <span>
+            <FaInbox />
+          </span>
+        </li>
+        <li>
+          <span>
+            <FaRegCalendarAlt />
+          </span>
+        </li>
+        <li>Next 7 days</li>
+      </div>
+      <div className="sidebar_+middle">
+        <span>
+          <FaChevronDown />
+        </span>
+        <h2>Projects</h2>
+      </div>
+      <ul className="Sidebar__Projects">Projects will be here</ul>
+    </div>
+  );
 };
